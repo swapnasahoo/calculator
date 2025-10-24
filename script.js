c1b13b0b90495334e064ff041360879fccfb74d8
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-10-25 02:40:31 +0530

    Added localStorage to save the calculation on the user's browser

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 const button = document.querySelectorAll('button');
 const calculationDisplay = document.querySelector('.calculation-display');
 
-let calculation = '';
+let calculation = localStorage.getItem('calculation') || '';
+calculationDisplay.value = calculation ? eval(calculation) : '';
 
 button.forEach((buttonElement) => {
   buttonElement.addEventListener('click', (e) => {
     if (e.target.innerHTML === '=') {
       calculationDisplay.value = eval(calculation);
+      localStorage.setItem('calculation', calculation);
     } else if (e.target.innerHTML === 'AC') {
       calculation = '';
       calculationDisplay.value = calculation;
+      localStorage.removeItem('calculation', calculation);
     } else if (e.target.innerHTML === 'DEL') {
       calculation = calculation.slice(0, -1);
       calculationDisplay.value = calculation;
     } else {
       calculation += e.target.innerHTML;
-      localStorage.setItem('calculation', calculation);
       calculationDisplay.value = calculation;
     }
   });
@@ -24,16 +26,12 @@ button.forEach((buttonElement) => {
 document.body.addEventListener('keydown', (e) => {
   if (e.key === '=') {
     calculationDisplay.value = eval(calculation);
+    localStorage.setItem('calculation', calculation);
   } else if (e.key === 'Enter') {
     calculationDisplay.value = eval(calculation);
-  } else if (e.key.innerHTML === 'AC') {
-    calculation = '';
-    calculationDisplay.value = calculation;
-  } else if (e.key === 'DEL') {
-    calculation = calculation.slice(0, -1);
-    calculationDisplay.value = calculation;
   } else if (e.key === 'Backspace') {
     calculation = calculation.slice(0, -1);
+    localStorage.setItem('calculation', calculation);
     calculationDisplay.value = calculation;
   } else if (e.key >= '0' && e.key <= 9) {
     calculation += e.key;
