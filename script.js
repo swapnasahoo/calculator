4b7bbe929937ae96b30ede2480a961da0d7d4b82
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-10-25 01:26:46 +0530

    Updated the calculator to be functional with keyboard keys as well

--- script.js
+++ script.js
@@ -19,3 +19,23 @@ button.forEach((buttonElement) => {
     }
   });
 });
+
+document.body.addEventListener('keydown', (e) => {
+  if (e.key === '=') {
+    calculationDisplay.value = eval(calculation);
+  } else if (e.key === 'Enter') {
+    calculationDisplay.value = eval(calculation);
+  } else if (e.key.innerHTML === 'AC') {
+    calculation = '';
+    calculationDisplay.value = calculation;
+  } else if (e.key === 'DEL') {
+    calculation = calculation.slice(0, -1);
+    calculationDisplay.value = calculation;
+  } else if (e.key === 'Backspace') {
+    calculation = calculation.slice(0, -1);
+    calculationDisplay.value = calculation;
+  } else {
+    calculation += e.key;
+    calculationDisplay.value = calculation;
+  }
+});
